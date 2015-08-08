d2d3891cc21bc91572318a45489f04c2ad870b46
Stephen Heron <stephen.heron@gmail.com>
2015-08-08 17:06:16 +0100

    Added event image

--- app/src/main/java/uk/co/stephenheron/giantbombupcoming/UpcomingRecyclerViewAdapter.java
+++ app/src/main/java/uk/co/stephenheron/giantbombupcoming/UpcomingRecyclerViewAdapter.java
@@ -1,10 +1,16 @@
 package uk.co.stephenheron.giantbombupcoming;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -29,7 +35,16 @@ public class UpcomingRecyclerViewAdapter extends RecyclerView.Adapter<UpcomingRe
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         Event event = upcomingEvents.get(position);
+
         viewHolder.textView.setText(event.getName());
+        setEventImage(viewHolder.imageView, event);
+    }
+
+    private void setEventImage(ImageView imageView, Event event)
+    {
+        Context context = imageView.getContext();
+        String imageUrl = event.getImageUrl().toString();
+        Picasso.with(context).load(imageUrl).into(imageView);
     }
 
     @Override
@@ -39,9 +54,12 @@ public class UpcomingRecyclerViewAdapter extends RecyclerView.Adapter<UpcomingRe
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
+        public ImageView imageView;
+
         public ViewHolder(View itemView) {
             super(itemView);
             this.textView = (TextView) itemView.findViewById(R.id.upcoming_event_info_text);
+            this.imageView = (ImageView) itemView.findViewById(R.id.upcoming_event_image);
         }
     }
 
